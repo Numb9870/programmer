199f9c3a116d45cd0d9cbb0f7ea8435fab5a2607
看不穿的是红尘 <fgy_game@163.com>
2025-03-05 12:41:41 +0800

    v1.0.30

---
+++ docs/2.前端/2-1.css/components/文字/WordShadow3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <p class="word-shadow">Word Shadow</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 200px;
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .word-shadow {
+    color: aquamarine;
+    font-size: 5rem;
+    text-shadow: -1px 1px #bbb, -2px 2px #bbb, -3px 3px #bbb, -4px 4px #bbb, -12px 12px #0009;
+  }
+}
+</style>
